d5c6e39d3406c8e6b60b27899e79a07b05314a53
Bertrand Lemasle <blemasle@gmail.com>
2013-10-06 15:55:21 +0000

    Configuration data structure > Basic settings #M800-15 Fixed

--- MLS800.h
+++ MLS800.h
@@ -1,3 +1,9 @@
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
 #ifndef _MLS800_h
 #define _MLS800_h
 
@@ -5,5 +11,15 @@
 #define DISPLAY_ADDR 0x20
 #define STORAGE_ADDR 0x20
 
+struct Config
+{
+	char seed[5];
+	char version[5];
+	byte rxChannel;
+	byte txChannel;
+	byte lastPath;
+	byte lastState;
+	byte displayDim;
+};
 
 #endif
